d12ddbf272a0a6b80c2b9f3c901ad1e5edcc63c8
CrhistanCastro <shinobi23senkun@gmail.com>
2025-11-01 18:54:28 -0400

    ex2 y ex3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magic = randomGenerator.Next(1,100);       
+
+        Console.Write("What is your guess? ");
+        string input = Console.ReadLine();
+        int guess = int.Parse(input);
+
+        while (guess != magic)
+        {
+            if (magic > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("Lower");
+            }
+            Console.Write("What is your guess? ");
+            input = Console.ReadLine();
+            guess = int.Parse(input);
+        }
+        Console.WriteLine("You guessed it!");
+        
     }
 }
