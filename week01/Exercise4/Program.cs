82d7f4357c5665570d7e985e82ae615666d90398
CrhistanCastro <shinobi23senkun@gmail.com>
2025-11-02 16:57:53 -0400

    ex4 y ex5

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,35 @@
 using System;
-
+using System.Collections.Generic;
+using System.Linq;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+
+        int num = -1;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while(num!=0)
+        {            
+            Console.Write("Enter a number: ");
+            num = int.Parse(Console.ReadLine());
+            if(num!=0)
+            {
+                numbers.Add(num);
+            }
+        }
+        Console.WriteLine($"The sum is: {numbers.Sum()}");
+        Console.WriteLine($"The average is: {numbers.Average()}");
+        Console.WriteLine($"The largest number is: {numbers.Max()}");
+        Console.WriteLine($"The smallest positive number is: {numbers.Where(n => n > 0).Min()}");
+        Console.WriteLine("The sorted list is: ");
+        numbers.Sort();
+        foreach(int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+        
     }
 }
