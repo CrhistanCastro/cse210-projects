6f6c23490d33714c6387e75f22c8ee0675cff967
CrhistanCastro <shinobi23senkun@gmail.com>
2025-12-01 21:44:00 -0400

    homework

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,5 +5,14 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Homework Project.");
+
+        Assignment assignment1 = new Assignment("David Coon", "Algebra");
+        MathAssignment math1 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        WritingAssignment writing1 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(assignment1.GetSummary());
+        Console.WriteLine(math1.GetSummary());
+        Console.WriteLine(math1.GetHomeworkList());
+        Console.WriteLine(writing1.GetSummary());
+        Console.WriteLine(writing1.GetWritingInformation());
     }
 }
