566162ca3a904cce55fa8fb6b31bc59fc63ed6e8
CrhistanCastro <shinobi23senkun@gmail.com>
2025-12-06 18:46:47 -0400

    Shape

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Console.WriteLine("Hello World! This is the Shapes Project.\n");
+
+        Square square1 = new Square("Blue", 6);
+        Rectangle rectangle1 = new Rectangle("Purple", 7, 3);
+        Circle circle1 = new Circle("Red", 9);
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square1);
+        shapes.Add(rectangle1);
+        shapes.Add(circle1);
+
+        foreach(Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+            Console.WriteLine($"The {color} shape has an area of {area}.\n");
+        }
     }
 }
